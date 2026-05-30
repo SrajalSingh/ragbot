9e2ce280c31ca11db9bf1a29936b31342975fa12
Srajal Singh <srajalsingh@Srajals-MacBook-Air.local>
2026-05-30 09:40:35 +0530

    fix: use langchain_classic.chains imports to fix Render startup crash

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 QA_CHAIN = None
